01530ef1c55a7421e4949a00e93b603de31f5f14
kavya.pl <kavya.pl@gmail.com>
2018-11-20 23:14:25 +0530

    linkedin auth fixed

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -2,37 +2,38 @@ const user = require("./../models/user.model");
 const axios = require("axios");
 const chalk = require("chalk");
 exports.linkedin = (req, res) => {
+  console.log(chalk.red("Calling..."));
   console.log(req.body.code);
-  axios
-    .post(
-      "https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code=" +
-        req.body.code +
-        "&redirect_uri=http://uiproductionenv.s3-website.us-east-2.amazonaws.com&client_id=81d483npdmnqhy&client_secret=bJDR1thqQ5OxNtHV"
-    )
-    .then(function(data) {
-      console.log("data==", data.data);
-      return axios.get("https://api.linkedin.com/v1/people/~?format=json", {
-        headers: {
-          Authorization: "Bearer " + data.data.access_token
-        }
+  try {
+    axios
+      .post(
+        "https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code=" +
+          req.body.code +
+          "&redirect_uri=http://uiproductionenv.s3-website.us-east-2.amazonaws.com&client_id=81d483npdmnqhy&client_secret=bJDR1thqQ5OxNtHV"
+        // "&redirect_uri=http://localhost:4200&client_id=81d483npdmnqhy&client_secret=bJDR1thqQ5OxNtHV"
+      )
+      .then(function(data) {
+        console.log("data==", data.data);
+        return axios.get(
+          "https://api.linkedin.com/v1/people/~:(id,firstName,lastName,headline,site-standard-profile-request,email-address)?format=json",
+          {
+            headers: {
+              Authorization: "Bearer " + data.data.access_token
+            }
+          }
+        );
+      })
+      .then(function(data) {
+        console.log("success", data);
+        res.send({ data: data.data });
+      })
+      .catch(function(err) {
+        throw err;
       });
-    })
-    .then(function(data) {
-      console.log("data==", data.data);
-      return axios.get("https://api.linkedin.com/v1/people/~?format=json", {
-        headers: {
-          Authorization: "Bearer " + data.data.access_token
-        }
-      });
-    })
-    .then(function(data) {
-      console.log("success", data);
-      res.send({ data: data.data });
-    })
-    .catch(function(err) {
-      console.log("Error");
-      res.send({ error: err });
-    });
+  } catch (err) {
+    console.log(chalk.red("Error", err));
+    res.send({ error: err });
+  }
 };
 
 exports.create = async (req, res) => {
